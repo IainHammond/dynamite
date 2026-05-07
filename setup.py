d567c77dc5a3baa8392acd51ca0d6da9f2de4fee
IainHammond <dr.iain.hammond@gmail.com>
2026-05-07 09:58:45 +0200

    delete old setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='dynamite',
-      description='DYNamical Analysis and MultIscale Tomography of line Emission',
-      url='http://github.com/cpinte/dynamite',
-      author='Christophe Pinte',
-      license='MIT',
-      packages=['dynamite'],
-      zip_safe=False)
